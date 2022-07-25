1eab0f56ecc071ca50bc687900d69841ec7a74ee
lector2003 <lector2003@gmail.com>
2022-07-25 10:40:36 -0300

    subida del contador

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import CartWidget from './components/CartWidget/CartWidget';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import ItemCount from './components/ItemCount/ItemCount';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import ItemCount from "./components/ItemCount/ItemCount";
 
 function App() {
-
-  const handleOnAdd = (quantity ) =>{
-    if(count <stock){
-      console.log('Cantidad de items agregados al carrito', quantity)
-    }
-    else{
-      console.log("Stock agotado")
-    }
-  } 
-  
+  const handleOnAdd = (quantity) => {
+    console.log("Cantidad de items agregados al carrito", quantity);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-      <video src="imagenNav/cafenav.mp4" autoplay="true" muted="true" loop="true"></video>
-        <Navbar/>
-        <ItemCount stock={15} inicial={1} onAdd={handleOnAdd}/>
-       <ItemListContainer greeting="Hola Mundo" />
+        <video
+          src="imagenNav/cafenav.mp4"
+          autoPlay={true}
+          muted={true}
+          loop={true}
+        ></video>
+        <Navbar />
+        <ItemCount stock={10} inicial={1} onAdd={handleOnAdd} />
+        <ItemListContainer greeting="Hola Mundo" />
       </header>
     </div>
   );
--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,31 +1,36 @@
-import { useState} from "react";
+import { useState } from "react";
 
-const ItemCount = ({inicial, stock, onAdd}) =>{
-    const [count, setCount] = useState(0)
+const ItemCount = ({ inicial, stock, onAdd }) => {
+  const [count, setCount] = useState(1);
 
-    const restar = () =>{
-        if(count>inicial){
-            setCount(count - 1)
-        }
-        
+  const restar = () => {
+    if (count > inicial) {
+      setCount(count - 1);
     }
+  };
 
-    const sumar = () =>{
-        if(count<stock){
-            setCount(count + 1)
-        }
+  const sumar = () => {
+    if (count < stock) {
+      setCount(count + 1);
     }
+  };
 
-    
+  const handleOnAdd = () => {
+    if (count > stock) {
+      onAdd(count);
+    } else {
+      console.log("Stock agotado");
+    }
+  };
 
-    return(
-        <div>
-            <button onClick={restar}>-</button>
-            <p>{count}</p>
-            <button onClick={sumar}>+</button>
-            <button onClick={()=>onAdd(count)}>Agregar al Carrito</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <button onClick={restar}>-</button>
+      <p>{count}</p>
+      <button onClick={sumar}>+</button>
+      <button onClick={handleOnAdd}>Agregar al Carrito</button>
+    </div>
+  );
+};
 
 export default ItemCount;
